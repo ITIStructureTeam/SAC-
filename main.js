453c5da582cfe8571b89daead6d9ce32c3f7b86b
islam <islammuhamed2019@gmail.com>
2021-06-15 20:20:10 +0200

    merge with read-json completed no errors observed

--- main.js
+++ main.js
@@ -555,64 +555,6 @@ if(gridLines == null){
 }
 
 
- 
-/* function GridSelections()
-{
-    let position = 0;
-    for (let i = 0; i <= listz.length; i++)
-    {
-        const text = "Z = "+ projUnits.LengthConvert(position, true);
-        position += listz[i];
-        $("#XY").append(`<option value=${position} >${text}</option>`);
-    }
-
-    position = 0;
-    for (let i = 0; i <= listy.length; i++)
-    {
-        const text = "Z = "+ projUnits.LengthConvert(position, true);
-        position += listy[i];
-        $("#XZ").append(`<option value=${position} >${text}</option>`);
-    }
-
-    position = 0;
-    for (let i = 0; i <= listx.length; i++)
-    {
-        const text = "Z = "+ projUnits.LengthConvert(position, true);
-        position += listx[i];
-        $("#YZ").append(`<option value=${position} >${text}</option>`);
-    }
-    
-    // const yz = document.getElementById("YZ");
-    // position = 0;
-    // for (let i = 0; i <= listx.length; i++)
-    // {
-    //     const option = document.createElement("option");
-    //     option.text = "X = "+position;
-    //     yz.add(option, yz[i]);
-    //     position += listx[i];
-    // }
-} */
-
-/* function removeSelectionGrids()
-{
-    const xy = $('#XY').children().length;
-    for (let i = xy - 1; i >= 0; i--) {
-        $('#XY').children()[i].remove(); 
-    }
-    
-    const xz = $('#XZ').children().length;
-    for (let i = xz - 1; i >= 0; i--) {
-        $('#XZ').children()[i].remove(); 
-    }
-    
-    const yz = $('#YZ').children().length;
-    for (let i = yz - 1; i >= 0; i--) {
-        $('#YZ').children()[i].remove(); 
-    }
-} */
-
- 
-
 document.getElementById("XYSection").onclick=function(){XYSection()};
 function XYSection()
 {
